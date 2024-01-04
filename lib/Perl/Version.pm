f3d331dfded0e6e0f0e3f1d69e650f3f39550e5f
brian d foy <briandfoy@pobox.com>
2024-01-04 09:56:21 -0500

    Update version in docs

--- lib/Perl/Version.pm
+++ lib/Perl/Version.pm
@@ -373,7 +373,7 @@ Perl::Version - Parse and manipulate Perl version strings
 
 =head1 VERSION
 
-This document describes Perl::Version version 1.014
+This document describes Perl::Version version 1.015
 
 =head1 SYNOPSIS
 
